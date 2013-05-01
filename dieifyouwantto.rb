80602f3f3e5ed7153aae22ad0a2062defc2fe969
wenoki <i@dec31.net>
2013-05-02 01:31:04 +0900

    じゃあ死ねよ

--- dieifyouwantto.rb
+++ dieifyouwantto.rb
@@ -1,24 +1,27 @@
 # encoding: utf-8
+
 require 'twitter'
 require 'tweetstream'
 
 Twitter.configure do |config|
-  config.consumer_key       = ENV['CONSUMER_KEY']
-  config.consumer_secret    = ENV['CONSUMER_SECRET']
-  config.oauth_token        = ENV['OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+  config.consumer_key       = ARGV[0]
+  config.consumer_secret    = ARGV[1]
+  config.oauth_token        = ARGV[2]
+  config.oauth_token_secret = ARGV[3]
 end
 
 TweetStream.configure do |config|
-  config.consumer_key       = ENV['CONSUMER_KEY']
-  config.consumer_secret    = ENV['CONSUMER_SECRET']
-  config.oauth_token        = ENV['OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+  config.consumer_key       = ARGV[0]
+  config.consumer_secret    = ARGV[1]
+  config.oauth_token        = ARGV[2]
+  config.oauth_token_secret = ARGV[3]
   config.auth_method        = :oauth
 end
 
 TweetStream::Client.new.userstream do |status|
-  if /死(にたい|んでしまいたい)[、。$¥z]/ =~ status.text
-  	puts status.text
+  if /死(にたい|んでしまいたい|んでしまった方が楽です)([、。ー〜・‥…!！¥(（]|$|¥z)/ =~ status.text && !status.text.include?("RT")
+    Twitter.update("@#{status.user.screen_name} じゃあ死ねよ。", :in_reply_to_status_id => status.id)
+  elsif status.text.include?("@dieifyouwantto") && !status.text.include?("RT")
+    Twitter.update("@#{status.user.screen_name} 死ねよ。", :in_reply_to_status_id => status.id)
   end
 end
